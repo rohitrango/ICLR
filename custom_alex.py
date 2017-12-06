e1c32b288175cb5e7b2f71b09cdbeb93703b1ade
rohitrango <rohit.rango@gmail.com>
2017-12-06 10:32:36 +0530

    sketch of B,C,D nets

--- custom_alex.py
+++ custom_alex.py
@@ -7,6 +7,8 @@ class CustomAlex(nn.Module):
 
     def __init__(self,model_id, num_classes=1000):
         super(CustomAlex,self).__init__()
+        self.model_id = model_id
+
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
@@ -22,6 +24,7 @@ class CustomAlex(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
+
         if model_id == "A":
             self.classifier = nn.Sequential(
                 nn.Dropout(),
@@ -33,23 +36,66 @@ class CustomAlex(nn.Module):
                 nn.Linear(4096,4096),
             )
 
-        if model_id == "B":
-            self.classifier = nn.Sequential(
-                nn.Dropout(),
-                nn.Linear(256 * 6 * 6, 4096),
-                nn.ReLU(inplace=True),
-                nn.Dropout(),
-                nn.Linear(4096,4096),
-            )
-        if model_id == "C" or model_id == "D":
-            self.classifier = nn.Sequential(
-                nn.Dropout(),
-                nn.Linear(256 * 6 * 6, 4096),
-            )
+        if model_id != "A":
+            # self.classifier = nn.Sequential(
+            #     nn.Dropout(),
+            #     nn.Linear(256 * 6 * 6, 4096),
+            #     nn.ReLU(inplace=True),
+            #     nn.Dropout(),
+            #     nn.Linear(4096,4096),
+            # )
+            # Define each layer and then define loss
+            # The layers to pass for B and C, D are the same
+            # With an extra FC-skip layer for D 
+            self.fc6 = nn.Sequential(
+                    nn.Dropout(),
+                    nn.Linear(256 * 6 * 6, 4096),
+                )
+            self.fc7 = nn.Sequential(
+                    nn.Dropout(),
+                    nn.Linear(4096, 4096),
+                )
+            self.fc8 = nn.Sequential(
+                    nn.Dropout(),
+                    nn.Linear(4096, 4096),
+                )
+
+
+        if model_id == "D":
+            # self.classifier = nn.Sequential(
+            #     nn.Dropout(),
+            #     nn.Linear(256 * 6 * 6, 4096),
+            # )
+            self.fc_skip = nn.Sequential(
+                    nn.Dropout(),
+                    nn.Linear(256 * 6 * 6, 4096),
+                )
         
 
     def forward(self, x):
         x = self.features(x)
         x = x.view(x.size(0), 256 * 6 * 6)
-        x = self.classifier(x)
-        return x
+
+        # The Linear layers will be different for different losses
+        if self.model_id == "A":
+            x = self.classifier(x)
+            return x
+
+        elif self.model_id == "B":
+            x = self.fc6(x)
+            penultimate = self.fc7(x)
+            output = self.fc8(x)
+            return penultimate, output
+
+        elif self.model_id == "C":
+            fc6 = self.fc6(x)
+            fc7 = self.fc7(fc6)
+            fc8 = self.fc8(fc7)
+            return fc6, fc7, fc8
+
+        else:
+            fc6 = self.fc6(x)
+            skip_fc6 = self.skip_fc6(x)
+            fc7 = self.fc7(fc6 + skip_fc6)
+            fc8 = self.fc8(fc7)
+            return fc_skip, fc7, fc8
